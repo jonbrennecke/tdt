a01edd87eaa29329e7b2ca35abd512915c8cc01b
Jon Brennecke <jpbrennecke@gmail.com>
2014-06-26 14:30:30 -0700

    working on API

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,25 +4,23 @@ requirejs.config({
 		jquery : "bower_components/jquery/dist/jquery.min",
 		jqueryui : "bower_components/jquery-ui/ui/minified/jquery-ui.min",
 		Q : "bower_components/q/q",
+		d3 : "bower_components/d3/d3.min",
 		tdt : "tdt",
 		ws : "ws",
+		figurejs : "figurejs/figure",
 	},
 
 	shim : {
-
 		jquery : {
 			exports : "$"
 		},
-
 		jqueryui : {
 			deps : ["jquery"],
 			exports : "$"
 		},
-
 		ws : {
 			deps : ['Q']
 		},
-
 		tdt : {
 			deps : [ 'Q', 'ws' ]
 		}
@@ -30,43 +28,66 @@ requirejs.config({
 });
 
 
-require([ "jqueryui", "tdt"],function ( $, TDT ) {
+require([ "tdt"],function ( TDT ) {
 
-	$(".draggable").draggable()
+	require([ "figurejs" ], function ( Figure ) {
 
-	var address = "10.237.221.204",
-		tdt = new TDT( address );
+		var figure = Figure("#graph");
+		
+		var data = (function(a,b){ while(b--) { a[b] = Math.random(); } return a})([],500);
 
-	tdt.getTankNames( function ( tanks ) {
+		figure.data.y = data;
+		figure.data.x = (function(a,b){ while(b--) { a[b] = b; } return a})([],data.length);
 
-		tanks = JSON.parse( tanks );
-		tanks.map( function ( tank ) {
-			$("<div class='tank' data-name='" + tank + "'>" + tank + "</div>")
-			.appendTo(".tanks")
-			.hover( function () {
-				// $(this).find(".blocks").show()
-			});
+		figure.attr({
+			margins : { top: 50, right: 50, bottom: 50, left: 50 },
 		});
 
-		$(".tank").each(function () {
+		figure.area();
 
-			var blocksDiv = $("<div class='blocks'></div>").appendTo(this)
+		// this.margins = { top: 50, right: 50, bottom: 50, left: 50 };
+		// this.width = this.domElement.width() - (this.margins.left + this.margins.right);
+		// this.height = this.domElement.height() - (this.margins.top + this.margins.bottom);
 
-			tdt.getBlockNames( this.dataset.name, function ( blocks ) {
-				console.log(blocks)
-				blocks = JSON.parse( blocks );
-				blocks.map( function ( block ) {
-					$(blocksDiv).append($("<div class='block' data-name='" + block + "'>" + block + "</div>"))
-				});
-			});
-		});
+
+
+	})
+
+	// $(".draggable").draggable()
+
+	// var address = "10.237.221.204",
+	// 	tdt = new TDT( address );
+
+	// tdt.getTankNames( function ( tanks ) {
+
+	// 	tanks = JSON.parse( tanks );
+	// 	tanks.map( function ( tank ) {
+	// 		$("<div class='tank' data-name='" + tank + "'>" + tank + "</div>")
+	// 		.appendTo(".tanks")
+	// 		.hover( function () {
+	// 			// $(this).find(".blocks").show()
+	// 		});
+	// 	});
+
+	// 	$(".tank").each(function () {
+
+	// 		var blocksDiv = $("<div class='blocks'></div>").appendTo(this)
+
+	// 		tdt.getBlockNames( this.dataset.name, function ( blocks ) {
+	// 			console.log(blocks)
+	// 			blocks = JSON.parse( blocks );
+	// 			blocks.map( function ( block ) {
+	// 				$(blocksDiv).append($("<div class='block' data-name='" + block + "'>" + block + "</div>"))
+	// 			});
+	// 		});
+	// 	});
 
 
 		// tdt.getStoreNames( { tank : tank, block : block } function ( storeNames ) {
 		// 	console.log( storeNames )
 		// });
 
-	});
+	// });
 
 
 })
